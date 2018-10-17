c12e78bb0f5629639cfab1c9d926e5af8a105ca3
Utkan Gezer <utkangezer@gmail.com>
2018-10-18 01:06:48 +0300

    Perhaps finished reading file properly?

--- source.c
+++ source.c
@@ -6,21 +6,85 @@
 typedef
 struct Formula_tag
 {
-	int * lits;
-	int * off_clauses;
-	int n_lits;
+	int n_vars;
 	int n_clauses;
+	int n_lits;
+	int ** occurlist;
+	int * off_clauses;
+	int * lits;
+	int i_lit;
 } Formula;
 
-
-Formula read(FILE * fp)
+Formula * new_formula(int n_vars, int n_clauses)
 {
-	Formula result = { NULL, NULL, 0, 0 };
+	Formula * result = malloc(sizeof * result);
 
+	result->n_vars = n_vars;
+	result->n_clauses = n_clauses;
+	result->n_lits = n_clauses * EXPLPC;
+
+	result->occurlist = calloc(result->n_vars, sizeof * result->occurlist);
+	// plus 1 is for algorithm's efficiency
+	result->off_clauses = calloc(result->n_clauses + 1, sizeof * result->off_clauses);
+	result->lits = malloc(result->n_lits * sizeof * result->lits);
+
+	result->i_lit = 0;
+
+	return result;
+}
+
+void del_formula(Formula * formula)
+{
+	for (int i = 0; i < formula->n_vars; i++)
+		if (formula->occurlist[i])
+			free(formula->occurlist[i]);
+
+	free(formula->occurlist);
+	free(formula->off_clauses);
+	free(formula->lits);
+
+	free(formula);
+}
+
+void occurlist_add(Formula * formula, int var, int offset)
+{
+	if (!formula->occurlist[var]) {
+		formula->occurlist[var] = malloc(EXPLPC * sizeof * formula->occurlist[var]);
+		formula->occurlist[var][0] = offset;
+		formula->occurlist[var][1] = 0;
+		formula->occurlist[var][EXPLPC - 1] = -1;
+		return;
+	}
+
+	int end = 0;
+	while (formula->occurlist[var][end++]);
+
+	formula->occurlist[var][end - 1] = offset;
+	if (formula->occurlist[var][end] == -1) {
+		formula->occurlist[var] = realloc(formula->occurlist[var], 2 * (end + 1) * sizeof * formula->occurlist[var]);
+		formula->occurlist[var][2 * end + 1] = -1;
+	}
+	formula->occurlist[var][end] = 0;
+}
+
+void lits_add(Formula * formula, int lit)
+{
+	if (formula->i_lit >= formula->n_lits) {
+		formula->n_lits *= 2;
+		formula->lits = realloc(formula->lits, formula->n_lits * sizeof * formula->lits);
+	}
+
+	occurlist_add(formula, abs(lit), formula->i_lit);
+	formula->lits[formula->i_lit++] = lit;
+}
+
+Formula * read(FILE * fp)
+{
 	char buffer[BUFSIZ];
 	char * bufptr;
 
 	int n_vars;
+	int n_clauses;
 
 	while (fgets(buffer, BUFSIZ, fp) != NULL)
 	{
@@ -29,30 +93,23 @@ Formula read(FILE * fp)
 		}
 		else if (*buffer == 'p') {
 			// the spec
-			if (sscanf(buffer, "p cnf %d %d", &n_vars, &result.n_clauses) != 2) {
+			if (sscanf(buffer, "p cnf %d %d", &n_vars, &n_clauses) != 2) {
 				fputs(stderr, "Error at the spec line.");
-				return result;
+				return NULL;
 			}
 			break;
 		}
 		else {
 			fputs(stderr, "Couldn't find spec line as the first non-comment line.\n");
-			return result;
+			return NULL;
 		}
 	}
 
-	result.n_lits = result.n_clauses * EXPLPC;
-
-	// memory for all the literals and clause offsets
-	result.lits = malloc(result.n_lits * sizeof * result.lits);
-	result.off_clauses = calloc(result.n_clauses, sizeof * result.off_clauses);
-
-	int * p_lit = result.lits;
-	int * lim_lit = p_lit + result.n_lits;
+	Formula * result = new_formula(n_vars, n_clauses);
 
 	int i_clause = 0;
 
-	while (i_clause < result.n_clauses && fgets(buffer, BUFSIZ, fp) != NULL)
+	while (i_clause < result->n_clauses && fgets(buffer, BUFSIZ, fp) != NULL)
 	{
 		if (*buffer == 'c') {
 			// a comment
@@ -60,42 +117,32 @@ Formula read(FILE * fp)
 		else {
 			// a clause
 			int i_lit = 0;
+			int lit;
 			char * token = strtok(buffer, " ");
 
 			while (token != NULL)
 			{
-				if (p_lit >= lim_lit) {
-					result.lits = realloc(result.lits, 2 * result.n_lits * sizeof * result.lits);
-					p_lit = result.lits + result.n_lits;
-					lim_lit = result.lits + 2 * result.n_lits;
-					result.n_lits *= 2;
-				}
-
-				*p_lit = atoi(token);
-				if (*p_lit == 0) {
-					break;
-				}
-
+				lit = atoi(token);
+				lits_add(result, lit);
 				token = strtok(NULL, " ");
-				p_lit++;
 				i_lit++;
 			}
 
-			result.off_clauses[i_clause] =
+			result->off_clauses[i_clause + 1] = result->off_clauses[i_clause] + i_lit;
 
 			i_clause++;
 		}
 	}
 
-	if (i_clause != result.n_clauses) {
-		fprintf(stderr, "%d/%d clauses are missing.\n", result.n_clauses - i_clause, result.n_clauses);
-		free(result.lits);
+	if (i_clause != result->n_clauses) {
+		fprintf(stderr, "%d/%d clauses are missing.\n", result->n_clauses - i_clause, result->n_clauses);
+		free(result->lits);
 		return result;
 	}
 
 	if (ferror(fp)) {
 		perror("Error reading file.");
-		free(result.lits);
+		free(result->lits);
 		return result;
 	}
 
