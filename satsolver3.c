fd7f7cab0e25ae7ea4995fe91162af0444108fcb
Utkan Gezer <utkangezer@gmail.com>
2018-10-27 21:01:34 +0300

    s3 does everything just fine, except 5

--- satsolver3.c
+++ satsolver3.c
@@ -2,49 +2,56 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
+#include <math.h>
 
 #define BUFFERSIZE 1024
 #define EXPLPC 4
 
 typedef uint32_t bitstore;
 
-#define sbitstore (8 * sizeof(bitstore))
-#define bit(x) (1U << (x))
-#define mask(x, y) ((x) & bit(y))
-#define sset(s, i) mask((s)[(i) / sbitstore], (i) % sbitstore)
+#define sbitstore      (8 * sizeof(bitstore))
+#define bit(x)         (1U << (x))
+#define mask(x, y)     ((x) & bit(y))
+#define is_s_set(s, i) mask((s)[(i) / sbitstore], (i) % sbitstore)
+#define s_set(s, i)    (s)[(i) / sbitstore] |= bit((i) % sbitstore)
 
-int n_clauses;
-int n_vars;
+
+size_t n_clauses;
+size_t n_vars;
 int ** clauses;
-int ** occurlists;
+bitstore ** occurlists;
 
-int c_conf_size;
-int ol_conf_size;
-int cfg_size;
-int cfg_byte_size;
+int n_lits;
+float mean_occ_len;
+
+size_t cconf_len;
+size_t olconf_len;
+size_t cfg_len;
+size_t cfg_size;
 
 void init_globals(void) {
-	c_conf_size = (n_clauses - 1) / sbitstore + 1;
-	ol_conf_size = n_vars / sbitstore + 1;
-	cfg_size = c_conf_size + 2 * ol_conf_size;
-	cfg_byte_size = cfg_size * sizeof(bitstore);
+	cconf_len = (n_clauses - 1) / sbitstore + 1;
+	olconf_len = n_vars / sbitstore + 1;
+	cfg_len = cconf_len + 2 * olconf_len;
+	cfg_size = cfg_len * sizeof(bitstore);
 }
 
 void init_formula(void)
 {
+	init_globals();
+
 	clauses = malloc(n_clauses * sizeof * clauses);
 	occurlists = malloc((2 * n_vars + 1) * sizeof * occurlists);
 	occurlists += n_vars;
+	occurlists[0] = NULL;
 
 	for (int i = 0; i < n_clauses; i++) {
 		clauses[i] = calloc(EXPLPC + 2, sizeof * clauses[i]);
 		clauses[i][EXPLPC + 1] = -1;
 	}
 	for (int i = 1; i <= n_vars; i++) {
-		occurlists[i] = malloc((n_clauses + 1) * sizeof * occurlists[i]);
-		occurlists[-i] = malloc((n_clauses + 1) * sizeof * occurlists[-i]);
-		occurlists[i][0] = 0;
-		occurlists[-i][0] = 0;
+		occurlists[i] = calloc(cconf_len, sizeof * occurlists[i]);
+		occurlists[-i] = calloc(cconf_len, sizeof * occurlists[-i]);
 	}
 }
 
@@ -69,9 +76,8 @@ void lits_add(int lit, int i_clause)
 	}
 	clause[clause[0]] = lit;
 
-	int * occurlist = occurlists[lit];
-	if (occurlist[0] && occurlist[occurlist[0]] == i_clause) return;
-	occurlist[++occurlist[0]] = i_clause;
+	s_set(occurlists[lit], i_clause);
+	n_lits++;
 }
 
 int read(FILE * fp)
@@ -85,7 +91,7 @@ int read(FILE * fp)
 		}
 		else if (*buffer == 'p') {
 			// the spec
-			if (sscanf(buffer, "p cnf %d %d", &n_vars, &n_clauses) != 2) {
+			if (sscanf(buffer, "p cnf %u %u", &n_vars, &n_clauses) != 2) {
 				fputs("Error at the spec line.\n", stderr);
 				return 0;
 			}
@@ -122,9 +128,10 @@ int read(FILE * fp)
 			i_clause++;
 		}
 	}
+	mean_occ_len = (float) n_lits / n_vars;
 
 	if (i_clause != n_clauses) {
-		fprintf(stderr, "%d/%d clauses are missing.\n", n_clauses - i_clause, n_clauses);
+		fprintf(stderr, "%u/%u clauses are missing.\n", n_clauses - i_clause, n_clauses);
 		return 0;
 	}
 
@@ -147,18 +154,16 @@ int lit_occurs_clause(int lit, int i_clause)
 
 void lit_propagate(bitstore * cconf, int lit)
 {
-	int * occurlist = occurlists[lit];
-	for (int i = 1; i <= occurlist[0]; i++) {
-		// mark the occurlist[i]th clause as satisfied
-		cconf[occurlist[i] / sbitstore] |= bit(occurlist[i] % sbitstore);
-	}
+	bitstore * occurlist = occurlists[lit];
+	for (int i = 0; i < cconf_len; i++)
+		cconf[i] |= occurlist[i];
 }
 
 void var_assign(bitstore * config, int var, int ispos)
 {
 	bitstore * cconf = config;
-	bitstore * nconf = config + c_conf_size;
-	bitstore * pconf = nconf + ol_conf_size;
+	bitstore * nconf = config + cconf_len;
+	bitstore * pconf = nconf + olconf_len;
 
 	bitstore * xconf;
 	int lit;
@@ -166,22 +171,38 @@ void var_assign(bitstore * config, int var, int ispos)
 	if (ispos) xconf = pconf, lit = var;
 	else       xconf = nconf, lit = -var;
 	
-	xconf[var / sbitstore] |= bit(var % sbitstore);
+	s_set(xconf, var);
+	lit_propagate(cconf, lit);
+}
+
+void lit_assign(bitstore * config, int lit)
+{
+	bitstore * cconf = config;
+	bitstore * nconf = config + cconf_len;
+	bitstore * pconf = nconf + olconf_len;
+
+	bitstore * xconf;
+	int var;
+
+	if (lit > 0) xconf = pconf, var = lit;
+	else         xconf = nconf, var = -lit;
+
+	s_set(xconf, var);
 	lit_propagate(cconf, lit);
 }
 
 int c_len_reductions(bitstore * config)
 {
 	bitstore * cconf = config;
-	bitstore * nconf = config + c_conf_size;
-	bitstore * pconf = nconf + ol_conf_size;
+	bitstore * nconf = config + cconf_len;
+	bitstore * pconf = nconf + olconf_len;
 
 	int last_edit = n_clauses;
 
 	for (int i = 0; i != last_edit; i++) {
 		if (i == n_clauses) i = 0;
-		// sset of cconf at i, signifies that the clause i is satisfied
-		if (sset(cconf, i)) continue;
+		// is_s_set of cconf at i, signifies that the clause i is satisfied
+		if (is_s_set(cconf, i)) continue;
 
 		int * clause = clauses[i];
 		int clen = 0;
@@ -192,9 +213,9 @@ int c_len_reductions(bitstore * config)
 			int ispos = lit > 0;
 			
 			// if the literal is set to its opposite, it is not there
-			// sset of nconf at var, signifies that var is set to False
-			// sset of pconf at var, signifies that var is set to True
-			if (!sset(ispos ? nconf : pconf, var)) {
+			// is_s_set of nconf at var, signifies that var is set to False
+			// is_s_set of pconf at var, signifies that var is set to True
+			if (!is_s_set(ispos ? nconf : pconf, var)) {
 				clen++;
 				u_var = var;
 				u_ispos = ispos;
@@ -213,10 +234,13 @@ int c_len_reductions(bitstore * config)
 
 unsigned int e_occurrence_unsat(bitstore * cconf, int lit)
 {
-	int * occurlist = occurlists[lit];
-	for (int i = occurlist[0]; i > 0; i--)
-		// return True if even one clause that it occurs is still not satisfied
-		if (!sset(cconf, occurlist[i]))
+	bitstore * occurlist = occurlists[lit];
+	for (int i = 0; i < cconf_len; i++)
+		if (occurlist[i] & ~cconf[i])
+			// 1111 0000 occurrences
+			// 0011 1100 satisfied clauses
+			// 1100 0011 ~satisfied clauses
+			// 1100 0000 occurrences unsatisfied
 			return 1U;
 	return 0U;
 }
@@ -228,14 +252,14 @@ unsigned int var_state(bitstore * cconf, int var)
 
 unsigned int ass_state(bitstore * nconf, bitstore * pconf, int var)
 {
-	return !!sset(nconf, var) << 1 | !!sset(pconf, var);
+	return !!is_s_set(nconf, var) << 1 | !!is_s_set(pconf, var);
 }
 
 void purity_reduction(bitstore * config)
 {
 	bitstore * cconf = config;
-	bitstore * nconf = config + c_conf_size;
-	bitstore * pconf = nconf + ol_conf_size;
+	bitstore * nconf = config + cconf_len;
+	bitstore * pconf = nconf + olconf_len;
 
 	int last_edit = n_vars + 1;
 
@@ -243,7 +267,7 @@ void purity_reduction(bitstore * config)
 		if (i == n_vars + 1) i = 1;
 
 		// skip the variables already set True/False
-		if (sset(pconf, i) || sset(nconf, i)) continue;
+		if (is_s_set(pconf, i) || is_s_set(nconf, i)) continue;
 
 		switch (var_state(cconf, i)) {
 			case 0b01:
@@ -265,26 +289,256 @@ int all_satisfied(bitstore * cconf)
 {
 	for (int i = 0; i < n_clauses; i++)
 		// return False if any clause left unsatisfied
-		if (!sset(cconf, i))
+		if (!is_s_set(cconf, i))
 			return 0;
 	return 1;
 }
 
 bitstore * copy_config(bitstore * config)
 {
-	bitstore * cfg = malloc(cfg_byte_size);
-	return memcpy(cfg, config, cfg_byte_size);
+	bitstore * cfg = malloc(cfg_size);
+	return memcpy(cfg, config, cfg_size);
 }
 
-int lit_choose(bitstore * config)
+int lit_choose_starting(bitstore * config, int n)
 {
-	bitstore * nconf = config + c_conf_size;
-	bitstore * pconf = nconf + ol_conf_size;
+	bitstore * nconf = config + cconf_len;
+	bitstore * pconf = nconf + olconf_len;
+
+	for (int i = n; i <= n_vars; i++)
+		if (ass_state(nconf, pconf, i) == 0b00)
+			return i;
+	for (int i = 1; i < n; i++)
+		if (ass_state(nconf, pconf, i) == 0b00)
+			return i;
+	return 0;
+}
+
+int lit_choose_first(bitstore * config)
+{
+	bitstore * nconf = config + cconf_len;
+	bitstore * pconf = nconf + olconf_len;
 
 	for (int i = 1; i <= n_vars; i++)
 		if (ass_state(nconf, pconf, i) == 0b00)
 			return i;
 	return 0;
+}
+
+int lit_choose_last(bitstore * config)
+{
+	bitstore * nconf = config + cconf_len;
+	bitstore * pconf = nconf + olconf_len;
+
+	for (int i = n_vars; i > 0; i++)
+		if (ass_state(nconf, pconf, i) == 0b00)
+			return i;
+	return 0;
+}
+
+// https://stackoverflow.com/a/109025/2736228
+int number_of_set_bits(uint32_t i)
+{
+	i = i - ((i >> 1) & 0x55555555);
+	i = (i & 0x33333333) + ((i >> 2) & 0x33333333);
+	return (((i + (i >> 4)) & 0x0F0F0F0F) * 0x01010101) >> 24;
+}
+
+int num_set_bits(bitstore s)
+{
+	int num = 0;
+	while (s) {
+		if (s & 1) num++;
+		s >>= 1;
+	}
+	return num;
+}
+
+int sat_count(bitstore * cconf)
+{
+	int c = 0;
+	for (int i = 0; i < cconf_len; i++)
+		c += number_of_set_bits(cconf[i]);
+	return c;
+}
+
+int lit_occurrence_count(bitstore * cconf, int lit)
+{
+	int c = 0;
+	bitstore * occurlist = occurlists[lit];
+
+	for (int i = 0; i < cconf_len; i++)
+		c += number_of_set_bits(~cconf[i] & occurlist[i]);
+
+	return c;
+}
+
+int var_occurrence_count(bitstore * cconf, int var)
+{
+	return
+		lit_occurrence_count(cconf, var) +
+		lit_occurrence_count(cconf, -var);
+}
+
+int lit_choose_max_occur_var(bitstore * config)
+{
+	bitstore * nconf = config + cconf_len;
+	bitstore * pconf = nconf + olconf_len;
+
+	int max = 0;
+	int max_i;
+
+	for (int i = 1; i <= n_vars; i++) {
+		if (ass_state(nconf, pconf, i) == 0b00) {
+			int n_occ = var_occurrence_count(config, i);
+			if (n_occ > max) {
+				max = n_occ;
+				max_i = i;
+			}
+		}
+	}
+
+	return max_i;
+}
+
+typedef
+struct pair_tag {
+	int a, b;
+} pair;
+
+pair lo_count_power(bitstore * config, int lit)
+{
+	bitstore * cconf = config;
+	bitstore * nconf = config + cconf_len;
+	bitstore * pconf = nconf + olconf_len;
+
+	int c = 0;
+	int p = 0;
+	bitstore * occurlist = occurlists[lit];
+
+	for (int i = 0; i < cconf_len; i++) {
+		bitstore occ = ~cconf[i] & occurlist[i];
+		int clause_i = i * 8 * sizeof occ;
+		while (occ) {
+			if (occ & 1U) {
+				c++;
+				int n_rest = 0;
+				int * clause = clauses[clause_i];
+				for (int j = 1; j <= clause[0]; j++) {
+					if (clause[j] != lit && ass_state(nconf, pconf, abs(clause[j])) == 0b00) {
+						n_rest++;
+					}
+				}
+				if (n_rest == 1) p++;
+			}
+			occ >>= 1;
+			clause_i++;
+		}
+	}
+
+	return (pair){ c, p };
+}
+
+int lit_choose_max_occur_power(bitstore * config)
+{
+	bitstore * nconf = config + cconf_len;
+	bitstore * pconf = nconf + olconf_len;
+
+	int max = -1;
+	int max_i;
+
+	for (int var = 1; var <= n_vars; var++)
+	if (ass_state(nconf, pconf, var) == 0b00) {
+		pair cp_pos = lo_count_power(config, var);
+		pair cp_neg = lo_count_power(config, -var);
+		
+		int unsat_count = n_clauses - sat_count(config);
+		int f = round(mean_occ_len * unsat_count / n_clauses);
+		int score_pos = cp_pos.a + f * cp_neg.b;
+		int score_neg = cp_neg.a + f * cp_pos.b;
+
+		int temp = score_pos;
+		score_pos += score_neg * 0.75;
+		score_neg += temp * 0.75;
+
+		if (score_pos > max) max = score_pos, max_i = var;
+		if (score_neg > max) max = score_neg, max_i = -var;
+	}
+
+	return max_i;
+}
+
+int lit_choose_min_occur_var(bitstore * config)
+{
+	bitstore * nconf = config + cconf_len;
+	bitstore * pconf = nconf + olconf_len;
+
+	int min = 2 * n_clauses;
+	int min_i;
+
+	for (int i = 1; i <= n_vars; i++) {
+		if (ass_state(nconf, pconf, i) == 0b00) {
+			int n_occ = var_occurrence_count(config, i);
+			if (n_occ < min) {
+				min = n_occ;
+				min_i = i;
+			}
+		}
+	}
+
+	return min_i;
+}
+
+int lit_choose_max_occur_lit(bitstore * config)
+{
+	bitstore * nconf = config + cconf_len;
+	bitstore * pconf = nconf + olconf_len;
+
+	int max = 0;
+	int max_i;
+
+	for (int i = 1; i <= n_vars; i++) {
+		if (ass_state(nconf, pconf, i) == 0b00) {
+			int n_occ_pos = var_occurrence_count(config, i);
+			int n_occ_neg = var_occurrence_count(config, -i);
+			if (n_occ_pos > max) {
+				max = n_occ_pos;
+				max_i = i;
+			}
+			if (n_occ_neg > max) {
+				max = n_occ_neg;
+				max_i = -i;
+			}
+		}
+	}
+
+	return max_i;
+}
+
+int lit_choose_min_occur_lit(bitstore * config)
+{
+	bitstore * nconf = config + cconf_len;
+	bitstore * pconf = nconf + olconf_len;
+
+	int min = n_clauses;
+	int min_i;
+
+	for (int i = 1; i <= n_vars; i++) {
+		if (ass_state(nconf, pconf, i) == 0b00) {
+			int n_occ_pos = var_occurrence_count(config, i);
+			int n_occ_neg = var_occurrence_count(config, -i);
+			if (n_occ_pos < min) {
+				min = n_occ_pos;
+				min_i = i;
+			}
+			if (n_occ_neg < min) {
+				min = n_occ_neg;
+				min_i = -i;
+			}
+		}
+	}
+
+	return min_i;
 }
 
 bitstore * dpll_rec(bitstore * config)
@@ -299,27 +553,27 @@ bitstore * dpll_rec(bitstore * config)
 	bitstore * configA = copy_config(config);
 	bitstore * configB = config;
 
-	int choice = lit_choose(config);
+	int choice = lit_choose_max_occur_power(config);
 	if (choice == 0) {
 		puts("This shouldn't happen.");
 		free(config);
 		return NULL;
 	}
 
-	var_assign(configA, choice, 1);
+	lit_assign(configA, choice);
 	configA = dpll_rec(configA);
 	if (configA) {
 		free(configB);
 		return configA;
 	}
 
-	var_assign(configB, choice, 0);
+	lit_assign(configB, -choice);
 	return dpll_rec(configB);
 }
 
 bitstore * dpll_depth(void)
 {
-	bitstore * config = calloc(c_conf_size + 2 * ol_conf_size, sizeof * config);
+	bitstore * config = calloc(cconf_len + 2 * olconf_len, sizeof * config);
 	return dpll_rec(config);
 }
 
@@ -343,11 +597,16 @@ dpll_result dpll_step(bitstore * config)
 
 bitstore * dpll_breadth(void)
 {
-	size_t size = 1U << 22;
-	bitstore * prealloc = malloc(size * cfg_byte_size);
-	dpll_result * results = malloc(size * sizeof * results);
+	size_t length = (1ULL << 30) / cfg_size;
+	bitstore * prealloc = malloc(length * cfg_size);
+	dpll_result * results = malloc(length * sizeof * results);
 
-	memset(prealloc, 0, cfg_byte_size);
+	if (prealloc == NULL) {
+		puts("malloc failed for prealloc");
+		return NULL;
+	}
+
+	memset(prealloc, 0, length * cfg_size);
 	results[0] = TBD;
 
 	int nTBD = 1;
@@ -356,11 +615,11 @@ bitstore * dpll_breadth(void)
 	// continue until all become FAILs and one becomes SUCCESS
 	// last will remain the same, if all none turned out TBD or SUCCESS
 	while (nTBD) {
-		if (nTBD < last / 2 || last > size / 2) {
+		if (nTBD < last / 2 || last > length / 2) {
 			int old_last = last;
 			last--;
 			for (int i = 0; i <= last; i++) if (results[i] == FAIL) {
-				memcpy(prealloc + i * cfg_size, prealloc + last-- * cfg_size, cfg_byte_size);
+				memcpy(prealloc + i * cfg_len, prealloc + last-- * cfg_len, cfg_size);
 				results[i] = TBD; // we know it's not FAIL nor SUCCESS
 				while (results[last] == FAIL) last--;
 			}
@@ -371,20 +630,20 @@ bitstore * dpll_breadth(void)
 		nTBD = 0;
 
 		for (int i = last - 1; i >= 0; i--) if (results[i] == TBD) {
-			bitstore * exhibit = prealloc + i * cfg_size;
+			bitstore * exhibit = prealloc + i * cfg_len;
 			bitstore * exhibitA;
 			bitstore * exhibitB;
 			int choice;
 
 			switch (results[i] = dpll_step(exhibit)) {
 				case TBD:
-					choice = lit_choose(exhibit);
+					choice = lit_choose_max_occur_var(exhibit);
 					if (choice != 0) {
 						exhibitA = exhibit;
-						exhibitB = memcpy(prealloc + last * cfg_size, exhibit, cfg_byte_size);
+						exhibitB = memcpy(prealloc + last * cfg_len, exhibit, cfg_size);
 
-						var_assign(exhibitA, choice, 1);
-						var_assign(exhibitB, choice, 0);
+						lit_assign(exhibitA, choice);
+						lit_assign(exhibitB, -choice);
 
 						results[last++] = TBD;
 						nTBD += 2;
@@ -412,8 +671,8 @@ bitstore * dpll_breadth(void)
 
 void print_assignments(bitstore * config, FILE * stream)
 {
-	bitstore * nconf = config + c_conf_size;
-	bitstore * pconf = nconf + ol_conf_size;
+	bitstore * nconf = config + cconf_len;
+	bitstore * pconf = nconf + olconf_len;
 
 	for (int i = 1; i <= n_vars; i++) {
 		switch (ass_state(nconf, pconf, i)) {
@@ -458,8 +717,7 @@ int main(int argc, char const *argv[])
 		return -1;
 	}
 
-	init_globals();
-	bitstore * config = dpll_breadth();
+	bitstore * config = dpll_depth();
 
 	if (config == NULL) {
 		puts("Unsatisfiable.");
